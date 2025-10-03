6e79662176d4e171bbd7d6447e6b88414c7f0e1a
Esteban Chacon Martin <etherbeing99@proton.me>
2025-10-03 18:25:40 -0400

    fix: Missing deps

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 python3 manage.py migrate
 
-mkdir -p /var/log/celery/
+# mkdir -p /var/log/celery/
 
-python3 -m celery  -A eagle_achievements_service_backend beat --logfile=/var/log/celery/beat.log --loglevel=info --pidfile=/tmp/celeryworker.pid --schedule=/tmp/celerybeat-schedule --detach
-python3 -m celery  -A eagle_achievements_service_backend worker --logfile=/var/log/celery/worker.log --loglevel=info --pidfile=/tmp/celerybeat.pid --schedule=/tmp/celerybeat-worker --detach
+# python3 -m celery  -A qu_security beat --logfile=/var/log/celery/beat.log --loglevel=info --pidfile=/tmp/celeryworker.pid --schedule=/tmp/celerybeat-schedule --detach
+# python3 -m celery  -A qu_security worker --logfile=/var/log/celery/worker.log --loglevel=info --pidfile=/tmp/celerybeat.pid --schedule=/tmp/celerybeat-worker --detach
 
 python3 manage.py collectstatic --noinput
 
-gunicorn eagle_achievements_service_backend.wsgi:application --bind :8000 --workers 4 --timeout 120
+python3 -m gunicorn qu_security.wsgi:application --bind :8000 --workers 4 --timeout 120
